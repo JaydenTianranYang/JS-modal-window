363e6883522d3591fdbe6ad79795a6887584fd56
Jayden Yang <jayden.yang@rea-group.com>
2025-03-16 22:25:22 +1100

    Add addEventListener to overlay

--- script.js
+++ script.js
@@ -10,11 +10,15 @@ for (let i = 0; i < btnsOpenModal.length; i++)
         console.log('Button clicked');
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
-    }
-    );
+    });
 
 btnCloseModal.addEventListener('click', function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-}
-);
+});
+
+overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
+
