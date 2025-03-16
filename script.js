edf15d9d241631d7bdd6f5f9dde10829c3d9cfec
Jayden Yang <jayden.yang@rea-group.com>
2025-03-16 22:29:54 +1100

    Keep opimizing the code

--- script.js
+++ script.js
@@ -5,19 +5,20 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', function () {
-        console.log('Button clicked');
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
+const openModal = function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
 
 const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
 
-btnCloseModal.addEventListener('click', closeModal);
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal);
 
+btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
